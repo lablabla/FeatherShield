f2fec37d1a51644aa9e58f4f918bc130e1154e1f
Carmel Barak <lablabla@gmail.com>
2025-09-03 23:08:53 +0300

    Updated generating device certs to use CA files stored in google secret manager

--- utilities/generate_device_certs.py
+++ utilities/generate_device_certs.py
@@ -5,21 +5,59 @@ import subprocess
 import tempfile
 import uuid
 
+from google.cloud import secretmanager_v1
+
 DEVICE_ID = "esp32-001"
 
-def generate_certs(ca_folder, out_folder, device_id):
+def access_secret_payload(project_id: str, secret_id: str, version_id: str) -> bytes:
+    """
+    Access the payload for the given secret version and return it.
+    """        
+    client = secretmanager_v1.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+
+    response = client.access_secret_version(request={"name": name})
+
+    payload = response.payload.data
+    return payload
+
+def create_file_from_secret(path: str, project_id: str, secret_id: str, version_id: str = "latest"):
+    secret_bytes = access_secret_payload(project_id, secret_id, version_id)
+    with open(path, "wb") as f:
+        f.write(secret_bytes)
+
+def generate_certs(out_folder, device_id):
+    if not os.environ.get("FEATHERSHIELD_PROJECT_ID"):
+        raise ValueError("FEATHERSHIELD_PROJECT_ID environment variable must be set.")
+    project_id = os.environ.get("FEATHERSHIELD_PROJECT_ID")
+
+    if not os.environ.get("FEATHERSHIELD_CA_CERT_SECRET_ID"):
+        raise ValueError("FEATHERSHIELD_CA_CERT_SECRET_ID environment variable must be set.")
+    ca_cert_secret_id = os.environ.get("FEATHERSHIELD_CA_CERT_SECRET_ID")
+
+    if not os.environ.get("FEATHERSHIELD_CA_KEY_SECRET_ID"):
+        raise ValueError("FEATHERSHIELD_CA_KEY_SECRET_ID environment variable must be set.")
+    ca_key_secret_id = os.environ.get("FEATHERSHIELD_CA_KEY_SECRET_ID")
+    
     # Generate temp folder
     tmp_folder = tempfile.mkdtemp()
+    ca_folder = os.path.join(tmp_folder, "ca")
+    os.makedirs(out_folder, exist_ok=True)
+    os.makedirs(ca_folder, exist_ok=True)
+    create_file_from_secret(f"{os.path.join(ca_folder, 'ca.crt')}", project_id, ca_cert_secret_id)
+    create_file_from_secret(f"{os.path.join(ca_folder, 'ca.key')}", project_id, ca_key_secret_id)
+    # Copy ca.crt to out_folder as we will need to copy it to the ESP32
+    shutil.copy(f"{os.path.join(ca_folder, 'ca.crt')}", f"{os.path.join(out_folder, 'ca.crt')}")
+
     # Generate client private key
     subprocess.run(["openssl", "genrsa", "-out", f"{out_folder}/client.key", "4096"], check=True)
     # Create client CSR
     subprocess.run(["openssl", "req", "-new", "-key", f"{out_folder}/client.key", "-out", f"{tmp_folder}/client.csr", "-subj", f"/CN={device_id}"])
     # Sign client certificate with the CA
-    # (This assumes ca.crt and ca.key are in a known location)
-    subprocess.run(["sudo", "openssl", "x509", "-req", "-in", f"{tmp_folder}/client.csr", "-CA", f"{ca_folder}/ca.crt", "-CAkey", f"{ca_folder}/ca.key", "-CAcreateserial", "-out", f"{out_folder}/client.crt", "-days", "3650", "-sha256"], check=True)
+    subprocess.run(["openssl", "x509", "-req", "-in", f"{tmp_folder}/client.csr", "-CA", f"{ca_folder}/ca.crt", "-CAkey", f"{ca_folder}/ca.key", "-CAcreateserial", "-out", f"{out_folder}/client.crt", "-days", "3650", "-sha256"], check=True)
     # Remove temp folder
     shutil.rmtree(tmp_folder)
 
 if __name__ == "__main__":
-    generate_certs("../ca", "../out", DEVICE_ID)
+    generate_certs("../out", DEVICE_ID)
     print(f"Generated certificates for {DEVICE_ID}")
--- utilities/new_device.py
+++ utilities/new_device.py
@@ -8,17 +8,15 @@ import uuid
 def main():
     parser = argparse.ArgumentParser(description="Provision a new device.")
     parser.add_argument("--device-id", type=str, required=True, help="Device ID")
-    parser.add_argument("--ca-folder", type=str, required=True, help="Path to the CA folder")
     parser.add_argument("--out-folder", type=str, required=True, help="Path to the output folder")
     parser.add_argument("--pop", type=str, default=str(uuid.uuid4()), help="Proof Of Possessiong string")
     args = parser.parse_args()
     device_id = args.device_id
-    ca_folder = args.ca_folder
     out_folder = args.out_folder
     pop = args.pop
-    generate_certs(ca_folder, out_folder, device_id)
+    generate_certs(out_folder, device_id)
     generate_qr_code(out_folder, device_id, pop)
-    generate_nvs_csv(ca_folder, out_folder, device_id, pop)
+    generate_nvs_csv(out_folder, device_id, pop)
 
 if __name__ == "__main__":
     main()
